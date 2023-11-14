e8addd35e8e33026837594cde8ded172e08a4911
Sonia-Baibou <s.baibou@ohmycode.io>
2023-11-14 11:13:09 +0100

    update api for test - not activated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_url: 'https://ma-ville-demo.ohmycode.io',
   eaubonne_url: 'https://www.eaubonne.fr',
-  GOOGLE_MAP_API_KEY: 'AIzaSyDSCxohpwG_1PZkW6j9hDMiM62rgmVrV2k',
+  GOOGLE_MAP_API_KEY: 'AIzaSyDSCxohpwG_1PZkW6j9hDMiM62rgmVrV2i',
 };
